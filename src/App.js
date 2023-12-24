55cf31ecd38eff0a15dad9daf26006042c3363fd
daniloslovic17 <daniloslovic17@gmail.com>
2023-12-24 19:48:43 +0100

    Particles

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import Particle from './components/Particle/Particle';
 
 function App() {
   return (
     <div className="App">
+      <Particle />
       <Navigation />
       <Logo />
       <Rank />
