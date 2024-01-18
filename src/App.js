55cd2aa2c4ba370f8df5c0bcc73fd27e8938c8b1
daniloslovic17 <daniloslovic17@gmail.com>
2024-01-18 14:49:32 +0100

    Final touch

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ class App extends Component {
         })
         .then(response => response.json())
         .then(count => {
-          this.setState(Object.assign(this.state.user, { entries: count }))
+          this.setState(Object.assign(this.state.user, { entries: count.entries }))
         })
       }
       this.displayFaceBox(this.calculateFaceLocation(response))
